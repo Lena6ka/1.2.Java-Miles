f726b84441f70e4fb4df3252b5aaad132d8c7109
Elena Tolokova <elena89-06@mail.ru>
2022-02-06 00:48:17 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketPrice = 1000;
+        int miles = 200;
+
+        int bonusMiles = ticketPrice/miles;
+        System.out.println("Начислено " + bonusMiles + " миль" );
+           }
+}
