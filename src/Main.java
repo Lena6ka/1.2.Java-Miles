aa8bf5853160982d3f81c2ea02432f9463b6e96b
Elena Tolokova <elena89-06@mail.ru>
2022-02-07 23:52:13 +0300

    Fixed bug

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         int ticketPrice = 1000;
-        int miles = 200;
+        int miles = 20;
 
         int bonusMiles = ticketPrice/miles;
         System.out.println("Начислено " + bonusMiles + " миль" );
