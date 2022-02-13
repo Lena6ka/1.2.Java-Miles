a89b5d0f206480c7c2f8c20e5d14be4762775c84
Elena Tolokova <elena89-06@mail.ru>
2022-02-13 23:22:07 +0300

    Formatted code

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
 
-        int ticketPrice = 1000;
+        int ticketPrice = 1700;
         int miles = 20;
 
-        int bonusMiles = ticketPrice/miles;
-        System.out.println("Начислено " + bonusMiles + " миль" );
-           }
+        int bonusMiles = ticketPrice / miles;
+        System.out.println("Начислено " + bonusMiles + " миль");
+    }
 }
